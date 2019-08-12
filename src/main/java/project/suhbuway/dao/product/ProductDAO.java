f279879cc41faef08f95b4964632083f40dbf571
hkarling <dantert@gmail.com>
2019-08-12 14:45:33 +0900

    feat: 맙소사...

---
+++ src/main/java/project/suhbuway/dao/product/ProductDAO.java
@@ -0,0 +1,24 @@
+package project.suhbuway.dao.product;
+
+import java.util.List;
+
+import project.suhbuway.dto.Product;
+
+public interface ProductDAO {
+
+    /**
+     * 해당되는 카테고리에 있는 메뉴 리스트와 정보를 반환한다.
+     * 
+     * @param category
+     * @return
+     */
+    public List<Product> selectProductsByCategory(String category);
+
+    /**
+     * 해당되는 id의 메뉴 정보를 반환한다.
+     * 
+     * @param id
+     * @return
+     */
+    public Product selectProductById(int id);
+}
