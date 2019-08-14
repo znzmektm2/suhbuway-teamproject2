374a156cd2b344c8b9b292e24c149deb7d78acbf
pois689 <poia689@naver.com>
2019-08-14 19:41:12 +0900

    Revert "살리자..."
    This reverts commit e93c71aed6f74f419520b6c6e3cd8242c22fddc6.

--- src/main/java/project/suhbuway/controller/UserController.java
+++ src/main/java/project/suhbuway/controller/UserController.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * �쑀��愿�由� 留듯븨�� �씠怨녹쑝濡�..
- * @author hkarling 우니ㅏ런미아ㅓ리ㅏㅇㄴ머리ㅏㅇㄴ멀
+ * 유저관리 맵핑은 이곳으로..
+ * @author hkarling
  */
 @Controller
 public class UserController {
 
     @RequestMapping("/user/login")
     public void login() {
-    	// 이ㅏㅓㄴㅁㅇㄹ
 	
     }
     
