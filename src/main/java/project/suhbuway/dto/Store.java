76ead3b8e21a543ab73d58dbf6c4c000a55b1d67
MJ <MJ@192.168.0.110>
2019-08-26 17:35:18 +0900

    1122

--- src/main/java/project/suhbuway/dto/Store.java
+++ src/main/java/project/suhbuway/dto/Store.java
@@ -10,7 +10,7 @@ public class Store {
 	private String storeAddress;
 	private String storePhone;
 	private String storeOwner;
-	private String storeSales;
+	private int storeSales;
 }
 
 /* store_id,store_password,store_name,store_address,store_phone,store_owner,store_sales */
